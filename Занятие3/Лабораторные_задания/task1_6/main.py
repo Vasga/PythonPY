72e73bebab56a0145a427f1f355046df0323cc4b
Vasga <vasga@ro.ru>
2022-03-30 21:15:46 +0300

    вши 23.03.2022

--- Занятие3/Лабораторные_задания/task1_6/main.py
+++ Занятие3/Лабораторные_задания/task1_6/main.py
@@ -1,5 +1,11 @@
 # TODO написать функцию для поиска необходимой суммы денег
-def summa_(salary, spend, month=10, rost_cen = 0.03)
+def summa_(salary, spend, month=10, rost_cen = 0.03):
+    su = 0
+    for n in range(month):
+        su += spend - salary
+        spend *= 1 + rost_cen
+    return su
+
 
 if __name__ == "__main__":
-    ...  # TODO вызвать функцию и проверить работоспособность
+    print(summa_(5000, 6000))  # TODO вызвать функцию и проверить работоспособность
--- Занятие3/Лабораторные_задания/task1_7/main.py
+++ Занятие3/Лабораторные_задания/task1_7/main.py
@@ -1,4 +1,5 @@
 # TODO написать функцию для поиска количества месяцев
+def prozit_(money_capital, salaru, spend, 0.05)
 
 
 if __name__ == "__main__":
